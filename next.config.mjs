8d58dda6ef202ce0bc9245c07c0242ec3304356e
jdhersin <j.dhersin@hotmail.fr>
2026-01-14 07:39:10 +0100

    main: fix next js config baseath

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/site-3w",
-  assetPrefix: "/site-3w/",
+  //basePath: "/site-3w",
+  assetPrefix: "/site-3w",
 };
 
 export default withNextIntl(nextConfig);
--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  basePath: "/site-3w",
-  assetPrefix: "/site-3w/",
+  //basePath: "/site-3w",
+  assetPrefix: "/site-3w",
 };
 
 export default withNextIntl(nextConfig);
